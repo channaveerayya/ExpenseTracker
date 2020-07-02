e6f21d9e8346e6204b20bb62a67cdd75505c2444
channaveerayya <channucs0009@gmail.com>
2020-07-02 16:11:04 +0530

    adding chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'package:ExpenceTracker/widgets/chart.dart';
+
 import './widgets/transaction_list.dart';
 import './widgets/new_transaction.dart';
 import 'package:flutter/material.dart';
@@ -53,6 +55,17 @@ class _MyHomePageState extends State<MyHomePage> {
     //     amount: 80.12,
     //     date: DateTime.now())
   ];
+
+  List<Transaction> get _recentTransactions {
+    return _userTransactions.where((tx) {
+      return tx.date.isAfter(
+        DateTime.now().subtract(
+          Duration(days: 7),
+        ),
+      );
+    }).toList();
+  }
+
   void _addNewtransaction(String txTitle, double txAmount) {
     final newTx = Transaction(
       id: DateTime.now().toString(),
@@ -98,14 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
           // mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Colors.cyan[300],
-                child: Text('Chart'),
-                elevation: 12,
-              ),
-            ),
+            Chart(_recentTransactions),
             TransactionList(_userTransactions),
           ],
         ),
